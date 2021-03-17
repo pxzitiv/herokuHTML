8c22cf4477b8b3c2436b13871e85b76d3cc7851b
pxzitiv <80686013+pxzitiv@users.noreply.github.com>
2021-03-17 19:50:02 +0200

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+@app.route('/home')
+def home():
+    return ('Welcome !')
+
 @app.route('/first')
 def first():
     return render_template('first.html')
