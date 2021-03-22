9c384a3abda224eefc50070cd0d28d723657cd5a
pxzitiv <80686013+pxzitiv@users.noreply.github.com>
2021-03-22 15:40:38 +0400

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+<html>
+  <head>  
+  </head>
+  <style>
+  body {background-color: #3CB371;}
+  p    {
+  font-family: Comic Sans MS;
+  font-size: 80%;}
+  </style>
+  <body>
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -12,3 +22,6 @@ def first():
 @app.route('/login')
 def login():
     return render_template('login.html')
+    </body>
+    </head>
+        
